2378226d6f903c6c2c4d795ac3376c283dace705
rodion1004 <rodionb.820@gmail.com>
2025-04-26 15:21:26 -0400

    change

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@
 import Link from "next/link";
 import { Share_Tech_Mono } from "next/font/google";
 import { useEffect } from "react";
-import TypewriterComponent from "typewriter-effect";
+// import TypewriterComponent from "typewriter-effect";
 import AOS from "aos";
 import "aos/dist/aos.css";
 // import { use } from "react";
+import { Typewriter } from "react-simple-typewriter";
 const font = Share_Tech_Mono({
   subsets: ["latin"],
   weight: "400",
@@ -26,22 +27,25 @@ export default function Home() {
     <div className={`${font.className} flex flex-col items-center max-w-[2520px] justify-center mx-auto h-screen text-center bg-black text-[#00ffcc]`}>
       <div>
         <h1 data-aos="zoom-in" className="text-5xl font-bold mb-[40px] animation">ELLI-PSE : Portail d'accès</h1>
-        <TypewriterComponent
-          options={{
-            strings: ["Bienvenue dans l'univers caché d'Alex. Choisissez votre point d'entrée."],
-            autoStart: true,
-            loop: true,
-            deleteSpeed: 50,
-            delay: 100,
-          }}
-
-          onInit={(typewriter) => {
-            typewriter
-              .typeString("Bienvenue dans l'univers d'Alex")
-              .pauseFor(1000)
-              .deleteAll()
-              .start();
-          }}
+        {/* <TypewriterComponent */}
+           {/* options={{ */}
+             {/* strings: ["Bienvenue dans l'univers caché d'Alex. Choisissez votre point d'entrée."], */}
+        {/* //     autoStart: true,
+        //     loop: true,
+        //     deleteSpeed: 50,
+        //     delay: 100,
+        //   }}
+        // /> */}
+        <Typewriter
+          words={['Bienvenue dans l\'univers caché d\'Alex. Choisissez votre point d\'entrée.']}
+          loop={1000000}
+          cursor
+          cursorStyle='|'
+          typeSpeed={100}
+          deleteSpeed={50}
+          delaySpeed={1000}
+          onLoopDone={() => console.log(`loop completed`)}
+          onType={() => console.log(`typed`)}
         />
       </div>
       <div className="flex flex-col mt-[70px] gap-4  ">
--- app/propos/page.tsx
+++ app/propos/page.tsx
@@ -3,23 +3,29 @@
 
 import Image from "next/image"
 import Link from "next/link"
-import TypewriterComponent from "typewriter-effect"
+// import TypewriterComponent from "typewriter-effect"
+import { Typewriter } from "react-simple-typewriter";
 export default function Propos() {
+
     return (
         <div className="text-[#ccc]  px-[20px] py-[60px] max-w-[800px] mx-auto text-center h-max-screen overflow-hidden bg-black flex flex-col items-center justify-center">
-            <div  data-aos="fade-up"
-     data-aos-duration="3000" className=" flex flex-col items-center ">
+            <div data-aos="fade-up"
+                data-aos-duration="3000" className=" flex flex-col items-center ">
                 <Image src="/logo.png" width={120} height={120} className="mb-[20px] h-auto w-auto" alt="Logo La Grande Ourse" />
-                
-                <h1 className="text-[36px] text-[#0f0] font-bold mb-[30px] text-shadow-[0_0_10px_#0f0]"><TypewriterComponent
-                    options={{
-                        strings: ["La Grande Ourse"],
-                        autoStart: true,
-                        loop: true,
-                        deleteSpeed: 50,
-                        delay: 100,
-                    }}
-                /></h1>
+
+                <h1 className="text-[36px] text-[#0f0] font-bold mb-[30px] text-shadow-[0_0_10px_#0f0]">
+                    <Typewriter
+                        words={['La Grande Ourse', 'La Grande Ourse', 'La Grande Ourse']}
+                        loop={1000000}
+                        cursor
+                        cursorStyle='|'
+                        typeSpeed={100}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        onLoopDone={() => console.log(`loop completed`)}
+                        onType={() => console.log(`typed`)}
+                    />
+                </h1>
                 <p className="text-[17px] mt-[20px]"><b className="mr-[7px]"> La Grande Ourse</b> est une boîte de production indépendante fondée par des passionnés d'histoires fortes, visuelles et engagées. Nous aimons raconter autrement, jouer avec les codes du cinéma, du digital, et du réel.</p>
                 <p>Notre ambition : créer des récits percutants, immersifs, où le spectateur devient acteur.</p>
                 <h2 className="text-[#0ff] text-[24px] mt-[40px] font-bold">Notre mission</h2>
